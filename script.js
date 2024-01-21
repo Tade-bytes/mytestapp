78ab7c3fb59d5d723b1299f4e31a93620cdbd12d
Iyanuoluwa <iyanuoluwaoluwatade@gmail.com>
2024-01-22 00:08:43 +0100

    hello world

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ axios.get('https://www.omdbapi.com/?s=all&type=movie&apikey=e95f2e70')
   });
   
 const parent = document.getElementById('video-container');
-parent.innerHTML += 'Hello World'
+parent.innerHTML += 'Hello Hello World'
 movies.map((movie) => {parent.innerHTML += `<div class="video-box">
 <img src=${movie.Poster} />
 <h2>${movie.Title}</h2>
